9e4684509a1a0ca5fc0bf16c2654cc723bd8802c
Venki1992kv <venki1992kv@gmail.com>
2025-11-19 23:34:31 +0530

    Delete server directory

--- server/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  secret: process.env.JWT_SECRET || "your-secret-key",
-  expiresIn: process.env.JWT_EXPIRY || "24h",
-  refreshExpiresIn: process.env.REFRESH_TOKEN_EXPIRY || "7d",
-  refreshSecret: process.env.REFRESH_TOKEN_SECRET || "your-refresh-token-secret-key"
-};
